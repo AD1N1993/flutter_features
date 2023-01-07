a1c84dc1fcc7065b12de1efca0382354c9e3035f
yurygrivickiy <yury.grivickiy@finespace.com>
2023-01-07 19:33:43 +0200

    feat(e): add image

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:lazy_load/page_storage_key_scroll.dart';
 
 void main() {
+  var a = 1;
+  a+=1;
+  print(a);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
